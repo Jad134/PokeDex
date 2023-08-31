a04b3607a13098c2b72ff2fbd2e401eda867d61b
Jad El Nader <Jadelnader99@gmail.com>
2023-08-31 23:12:11 +0200

    Add More Pokemons function

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ let loadedPokemonImg = [];
 let trait1 = [];
 let trait2 = [];
 
+let startAmount = 1;
+
+
 async function init() {
      await loadPokemon();
-     renderPokemon();
-      
+
 }
 
+
+
 async function loadPokemon() {
-     for (i = 1; i < 181; i++) {
+
+     for (let i = startAmount ; i < startAmount + 20; i++) {
           let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
           let response = await fetch(url);
           let currentPokemon = await response.json();
@@ -20,34 +25,44 @@ async function loadPokemon() {
           trait1.push(currentPokemon['types']['0']['type']['name'])
 
           const firstType = currentPokemon['types'][0]['type']['name'];
-          console.log("Erster Typ:", firstType); // Eigenschaften der Pokemons
+          // console.log("Erster Typ:", firstType); // Eigenschaften der Pokemons
 
           if (currentPokemon['types'].length > 1) {
                // Zugriff auf den zweiten Typ
                const secondType = currentPokemon['types'][1]['type']['name'];
                trait2.push(secondType)
-             } else {
+          } else {
                trait2.push(null);
-             }
+          }
      }
+
      FirstLetterUpperCase()
+     renderPokemon();
 }
 
-function FirstLetterUpperCase(){ // Anfangsbuchstaben der Pokemon gross schreiben.
-     for(var i = 0 ; i < loadedPokemonName.length ; i++){
+async function loadMore() { // WICHTIG! Reihenfolge beachten. Erst startAmount und dann loadPokemon()
+     startAmount += 20;
+     await loadPokemon()
+     
+}
+
+
+function FirstLetterUpperCase() { // Anfangsbuchstaben der Pokemon gross schreiben.
+     for (var i = 0; i < loadedPokemonName.length; i++) {
           loadedPokemonName[i] = loadedPokemonName[i].charAt(0).toUpperCase() + loadedPokemonName[i].substr(1);
-      }
-       
+     }
+
 }
 
 
-function renderPokemon() {
-     for (let i = 0; i < loadedPokemonName.length; i++) {
+async function renderPokemon() {
+     for (let i = startAmount -1; i < startAmount + 18; i++) {
           let currentPokemonName = loadedPokemonName[i];
           let currentPokemonImg = loadedPokemonImg[i];
           let currentTrait1 = trait1[i];
           let currentTrait2 = trait2[i];
-          
+
+
           document.getElementById('show-pokemon').innerHTML += /*html*/`
                <div class="pokemon-container" id="pokemon-container${i}"> <h3>${currentPokemonName}</h3>
                  <div class="imgAndTrait">
@@ -60,13 +75,18 @@ function renderPokemon() {
           setBackgroundColor(i, currentTrait1);
           setFirstTraitColor(i, currentTrait1);
           setSecondTraitColor(i, currentTrait2);
-          
+
+
 
      }
+     console.log(loadedPokemonName);
+     console.log(loadedPokemonImg);
+     console.log(trait1);
+     console.log(trait2);
 }
 
 
- 
+
 
 function setBackgroundColor(i, currentTrait1) {
      const backgroundColors = {
@@ -75,7 +95,7 @@ function setBackgroundColor(i, currentTrait1) {
           'fairy': 'rgb(234, 75, 157)', 'fighting': 'rgb(90, 5, 5)', 'rock': 'rgb(50, 43, 43)',
           'psychic': 'rgb(122, 113, 113)', 'ghost': 'rgb(174, 140, 255)', 'ice': 'rgb(186, 242, 239)',
           'dragon': 'rgb(255, 214, 139)'
-      };
+     };
 
      const backgroundColor = backgroundColors[currentTrait1];
      if (backgroundColor) {
@@ -83,27 +103,29 @@ function setBackgroundColor(i, currentTrait1) {
      }
 }
 
-function setFirstTraitColor(i, currentTrait1){
+function setFirstTraitColor(i, currentTrait1) {
      const backgroundColors = {
           'fire': 'rgb(242, 56, 56)', 'normal': 'rgb(94, 92, 92)', 'water': 'rgb(53, 75, 242)', 'grass': 'rgb(48, 240, 67)',
           'bug': 'rgb(37, 125, 25)', 'poison': 'rgb(142, 49, 235)', 'electric': 'yellow', 'ground': 'rgb(145, 76, 10)',
           'fairy': 'rgb(232, 60, 203)', 'fighting': 'rgb(227, 32, 64)', 'rock': 'rgb(52, 54, 40)',
           'psychic': 'rgb(137, 153, 17)', 'ghost': 'rgb(189, 166, 157)', 'ice': 'rgb(71, 252, 255)',
           'dragon': 'rgb(237, 142, 33)'
-      };
-      let backgroundColor = backgroundColors[currentTrait1];
-      if (backgroundColor){
+     };
+     let backgroundColor = backgroundColors[currentTrait1];
+     if (backgroundColor) {
           document.getElementById(`traits${i}`).style.backgroundColor = backgroundColor;
-      }
+     }
 }
-function setSecondTraitColor(i, currentTrait2){
+function setSecondTraitColor(i, currentTrait2) {
      const backgroundColors = {
           'flying': 'rgb(50, 228, 237)', 'poison': 'rgb(142, 49, 235)', 'ground': 'rgb(145, 76, 10)', 'fairy': 'rgb(232, 60, 203)', 'ice': 'rgb(71, 252, 255)'
      }
      let backgroundColor = backgroundColors[currentTrait2];
-     if(backgroundColor){
+     if (backgroundColor) {
           document.getElementById(`secondTrait${i}`).style.backgroundColor = backgroundColor;
      }
 }
+
+
 
 

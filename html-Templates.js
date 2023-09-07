b54c6bc7cc11cca435c2130e34de6bc6508633c2
Jad El Nader <Jadelnader99@gmail.com>
2023-09-07 21:20:36 +0200

    Clean Code and final Works

---
+++ html-Templates.js
@@ -0,0 +1,55 @@
+function showCards(i, currentPokemonName,  currentPokemonImg,  currentTrait1, currentTrait2, currentId, currentHeight, currentWeight){
+    return /*html*/`
+    <div id="flip-card${i}" class="flip-card">
+        <div class="flip-card-inner">
+            <div  class="pokemon-container flip-card-front " id="pokemon-container${i}">
+                <h3>${currentPokemonName}</h3>
+                <div class="imgAndTrait">
+                    <img src="${currentPokemonImg}" id="poke-img">
+                    <span class="traits" id="traits${i}">${currentTrait1}</span>
+                    ${currentTrait2 !== null ? `<span class="traits" id="secondTrait${i}">${currentTrait2}</span>` : ''}
+    
+                </div>
+    
+            </div>
+              <div onclick="openCard(${i}) " id="flip-card-back${i}" class="flip-card-back">
+                  <div class="back-card-header"> 
+                    <h1>${currentPokemonName}</h1> 
+                    </div>
+                    <div class="back-card-mainInfo">
+                     <div id="id-container">
+                        <h2>Type</h2>
+                        <span class="id-number"> # ${currentId}</span>
+                     </div>
+                     <span id="backcard-trait${i}" class="backcard-trait">${currentTrait1}</span>
+                     ${currentTrait2 !== null ? `<span id="backcard-second-trait${i}" class="backcard-trait">${currentTrait2}</span>` : ''}
+                    </div>
+                      <div class="back-card-second-info">
+                        <span>Height: <span class="height-style"> ${currentHeight} m</span> </span>
+                        <span>Weight: <span class="height-style"> ${currentWeight} kg</span> </span>
+                       
+                      </div>
+              </div>
+    
+        </div>
+    </div> `;
+}
+
+function showOpenCards(currentPokemonName, currentPokemonImg, currentTrait1, currentTrait2, currentId, moves, showMoves){
+    return /*html*/`
+    <div class="open-card-header">
+        <h1>${currentPokemonName}</h1>
+    </div>
+    <div class="stats-and-img">
+     <div class="d-none" >${currentTrait1}</div>
+      <img class="open-cardImg" src="${currentPokemonImg}" alt="${currentPokemonName}">
+      <div class="stats">
+         <canvas id="myChart"></canvas>
+      </div>
+
+    </div>
+    <span class="section-moves">Moves</span>
+    <div class="move-container">
+         <div id="moves">${showMoves}</div>
+    </div> `;
+}
--- script.js
+++ script.js
@@ -16,92 +16,99 @@ async function init() {
 }
 
 async function loadPokemon() {
-     document.getElementById('pokeball-loader').classList.add('d-flex');
-     const loadMoreBtn = document.getElementById('load-more-btn');
-     loadMoreBtn.disabled = true;
-
+     loadingScreenOpen()
+     
      for (let i = startAmount; i < startAmount + 40; i++) {
           let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
           let response = await fetch(url);
           let currentPokemon = await response.json();
 
-          const stats = currentPokemon.stats.map(stat => ({
-               name: stat.stat.name,
-               value: stat.base_stat
-          }));
-
-          const moves = currentPokemon.moves.map(move => ({
-               name: move.move.name
-          }));
-
-
-          pokemonMoves.push(moves);
-
-          pokemonStats.push(stats);
-
-          loadedPokemonName.push(currentPokemon['name'])
-          loadedPokemonImg.push(currentPokemon['sprites']['other']['dream_world']['front_default'])
-          trait1.push(currentPokemon['types']['0']['type']['name'])
-          pokeId.push(currentPokemon['id'])
-          height.push(currentPokemon['height'])
-          weight.push(currentPokemon['weight'])
-
-          const firstType = currentPokemon['types'][0]['type']['name'];
-          // console.log("Erster Typ:", firstType); // Eigenschaften der Pokemons
-
-          if (currentPokemon['types'].length > 1) {
-               // Zugriff auf den zweiten Typ
-               const secondType = currentPokemon['types'][1]['type']['name'];
-               trait2.push(secondType)
-          } else {
-               trait2.push(null);
-          }
-
+          searchStatsAndMoves(currentPokemon)
+          pushPropetisToArray(currentPokemon)
      }
-     loadMoreBtn.disabled = false;
-     document.getElementById('pokeball-loader').classList.remove('d-flex');
+     loadingScreenEnd()
      FirstLetterUpperCase()
      renderPokemon();
 }
 
+function loadingScreenOpen(){
+     document.getElementById('pokeball-loader').classList.add('d-flex');
+     const loadMoreBtn = document.getElementById('load-more-btn');
+     loadMoreBtn.disabled = true;
+}
 
+function loadingScreenEnd(){
+     const loadMoreBtn = document.getElementById('load-more-btn');
+     loadMoreBtn.disabled = false;
+     document.getElementById('pokeball-loader').classList.remove('d-flex');
+}
+
+function searchStatsAndMoves(currentPokemon) {
+     const stats = currentPokemon.stats.map(stat => ({
+          name: stat.stat.name,
+          value: stat.base_stat
+     }));
+
+     const moves = currentPokemon.moves.map(move => ({
+          name: move.move.name
+     }));
+     pokemonMoves.push(moves);
+     pokemonStats.push(stats);
+}
+
+function pushPropetisToArray(currentPokemon) {
+     loadedPokemonName.push(currentPokemon['name'])
+     loadedPokemonImg.push(currentPokemon['sprites']['other']['dream_world']['front_default'])
+     trait1.push(currentPokemon['types']['0']['type']['name'])
+     pokeId.push(currentPokemon['id'])
+     height.push(currentPokemon['height'])
+     weight.push(currentPokemon['weight'])
+
+     const firstType = currentPokemon['types'][0]['type']['name'];
+     if (currentPokemon['types'].length > 1) {
+          // Zugriff auf den zweiten Typ
+          const secondType = currentPokemon['types'][1]['type']['name'];
+          trait2.push(secondType)
+     } else {
+          trait2.push(null);
+     }
+}
 
 function filterNames() {
      let search = document.getElementById('search').value.toLowerCase();
      let filteredPokemon = [];
 
-     for(let j = 0; j < loadedPokemonName.length; j++){
-          if(loadedPokemonName[j].toLowerCase().includes(search)){
+     for (let j = 0; j < loadedPokemonName.length; j++) {
+          if (loadedPokemonName[j].toLowerCase().includes(search)) {
                filteredPokemon.push(j);
           }
      }
+     showFilteredPokemon(filteredPokemon)
+}
 
-     for(let i = 0; i < loadedPokemonName.length; i++){
+function showFilteredPokemon(filteredPokemon) {
+     for (let i = 0; i < loadedPokemonName.length; i++) {
           let pokemonContainer = document.getElementById(`pokemon-container${i}`);
           let flipCard = document.getElementById(`flip-card${i}`)
           if (filteredPokemon.includes(i)) {
                pokemonContainer.style.display = ''; // Zeige das Pokémon
                flipCard.style.display = '';
-               
           } else {
-               
                flipCard.style.display = 'none'; // Verstecke das Pokémon
           }
      }
-     
 }
 
 async function loadMore() { // WICHTIG! Reihenfolge beachten. Erst startAmount und dann loadPokemon()
      startAmount += 40;
      await loadPokemon()
-
+     
 }
 
 function FirstLetterUpperCase() { // Anfangsbuchstaben der Pokemon gross schreiben.
      for (var i = 0; i < loadedPokemonName.length; i++) {
           loadedPokemonName[i] = loadedPokemonName[i].charAt(0).toUpperCase() + loadedPokemonName[i].substr(1);
      }
-
 }
 
 async function renderPokemon() {
@@ -114,48 +121,11 @@ async function renderPokemon() {
           let currentHeight = formatNumbers(height[i].toFixed());
           let currentWeight = formatNumbers(weight[i].toFixed());
 
-          document.getElementById('show-pokemon').innerHTML += /*html*/`
-<div id="flip-card${i}" class="flip-card">
-    <div class="flip-card-inner">
-        <div  class="pokemon-container flip-card-front " id="pokemon-container${i}">
-            <h3>${currentPokemonName}</h3>
-            <div class="imgAndTrait">
-                <img src="${currentPokemonImg}" id="poke-img">
-                <span class="traits" id="traits${i}">${currentTrait1}</span>
-                ${currentTrait2 !== null ? `<span class="traits" id="secondTrait${i}">${currentTrait2}</span>` : ''}
-
-            </div>
-
-        </div>
-          <div onclick="openCard(${i}) " id="flip-card-back${i}" class="flip-card-back">
-              <div class="back-card-header"> 
-                <h1>${currentPokemonName}</h1> 
-                </div>
-                <div class="back-card-mainInfo">
-                 <div id="id-container">
-                    <h2>Type</h2>
-                    <span class="id-number"> # ${currentId}</span>
-                 </div>
-                 <span id="backcard-trait${i}" class="backcard-trait">${currentTrait1}</span>
-                 ${currentTrait2 !== null ? `<span id="backcard-second-trait${i}" class="backcard-trait">${currentTrait2}</span>` : ''}
-                </div>
-                  <div class="back-card-second-info">
-                    <span>Height: <span class="height-style"> ${currentHeight} m</span> </span>
-                    <span>Weight: <span class="height-style"> ${currentWeight} kg</span> </span>
-                   
-                  </div>
-          </div>
-
-    </div>
-</div>
-          
-               `;
-
+          document.getElementById('show-pokemon').innerHTML += showCards(i, currentPokemonName, currentPokemonImg, currentTrait1, currentTrait2, currentId, currentHeight, currentWeight);
           setBackgroundColor(i, currentTrait1);
           setFirstTraitColor(i, currentTrait1);
           setSecondTraitColor(i, currentTrait2);
      }
-
 }
 
 
@@ -174,7 +144,6 @@ function setBackgroundColor(i, currentTrait1) {
      if (backgroundColor) {
           document.getElementById(`pokemon-container${i}`).style.backgroundColor = backgroundColor;
           document.getElementById(`flip-card-back${i}`).style.backgroundColor = backgroundColor;
-
      }
 }
 
@@ -183,8 +152,7 @@ function setFirstTraitColor(i, currentTrait1) {
           'fire': 'rgb(242, 56, 56)', 'normal': 'rgb(94, 92, 92)', 'water': 'rgb(53, 75, 242)', 'grass': 'rgb(48, 240, 67)',
           'bug': 'rgb(37, 125, 25)', 'poison': 'rgb(142, 49, 235)', 'electric': 'yellow', 'ground': 'rgb(145, 76, 10)',
           'fairy': 'rgb(232, 60, 203)', 'fighting': 'rgb(227, 32, 64)', 'rock': 'rgb(52, 54, 40)',
-          'psychic': 'rgb(137, 153, 17)', 'ghost': 'rgb(189, 166, 157)', 'ice': 'rgb(71, 252, 255)',
-          'dragon': 'rgb(237, 142, 33)'
+          'psychic': 'rgb(137, 153, 17)', 'ghost': 'rgb(189, 166, 157)', 'ice': 'rgb(71, 252, 255)', 'dragon': 'rgb(237, 142, 33)'
      };
      let backgroundColor = backgroundColors[currentTrait1];
      if (backgroundColor) {
@@ -214,7 +182,6 @@ function formatNumbers(height) {
 }
 
 function openCard(i) {
-     console.log(`openCard${i + 1} `)
      const currentPokemonName = loadedPokemonName[i];
      const currentPokemonImg = loadedPokemonImg[i];
      const currentTrait1 = trait1[i];
@@ -222,29 +189,14 @@ function openCard(i) {
      const currentId = pokeId[i];
      const moves = pokemonMoves[i];
      const showMoves = moves.map(move => `<span class="move">${move.name}</span>`).join(' ');  // Damit kann man Leerzeichen zwischen den moves platzieren und jedes move bekommt ein span um ihn zu stylen.
-
      const popupContent = document.getElementById('popup-pokemon');
-     popupContent.innerHTML = /*html*/`
-     <div class="open-card-header">
-         <h1>${currentPokemonName}</h1>
-     </div>
-     <div class="stats-and-img">
-      <div class="d-none" >${currentTrait1}</div>
-       <img class="open-cardImg" src="${currentPokemonImg}" alt="${currentPokemonName}">
-       <div class="stats">
-          <canvas id="myChart"></canvas>
-       </div>
- 
-     </div>
-     <span class="section-moves">Moves</span>
-     <div class="move-container">
-          <div id="moves">${showMoves}</div>
-     </div>
-         
-         <!-- Weitere Informationen zum Pokemon hier einfügen -->
-     `;
 
-     setTimeout(() => {
+     popupContent.innerHTML = showOpenCards(currentPokemonName, currentPokemonImg, currentTrait1, currentTrait2, currentId, moves, showMoves);
+     popupSettings(i, currentTrait1)
+}
+
+function popupSettings(i, currentTrait1) {
+     setTimeout(() => { // sonst wird die Chartfunktion zu früh gestartet und funktioniert nicht
           renderChart(i);
      }, 0);
 
@@ -255,8 +207,8 @@ function openCard(i) {
 
      modalContainer.style.display = 'block';
      overlay.style.display = 'block';
-
 }
+
 
 function closeModal() {
      const modalContainer = document.getElementById('modal-container');
@@ -273,8 +225,7 @@ function setModalBackgroundColor(trait) {
           'fire': 'red', 'normal': 'grey', 'water': 'blue', 'grass': 'green',
           'bug': 'darkgreen', 'poison': 'purple', 'electric': 'rgb(230, 247, 124)', 'ground': 'brown',
           'fairy': 'rgb(234, 75, 157)', 'fighting': 'rgb(90, 5, 5)', 'rock': 'rgb(50, 43, 43)',
-          'psychic': 'rgb(122, 113, 113)', 'ghost': 'rgb(174, 140, 255)', 'ice': 'rgb(186, 242, 239)',
-          'dragon': 'rgb(255, 214, 139)'
+          'psychic': 'rgb(122, 113, 113)', 'ghost': 'rgb(174, 140, 255)', 'ice': 'rgb(186, 242, 239)', 'dragon': 'rgb(255, 214, 139)'
      };
      const backgroundColor = backgroundColors[trait];
 
